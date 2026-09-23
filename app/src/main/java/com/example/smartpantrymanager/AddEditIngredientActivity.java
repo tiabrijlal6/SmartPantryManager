7384be929b7342c12437690ed9f87e40639129cf
Tia Brijlal <tiabrijlal6@gmail.com>
2026-09-23 22:25:55 +0200

    Add ingredient form and navigation

--- app/src/main/java/com/example/smartpantrymanager/AddEditIngredientActivity.java
+++ app/src/main/java/com/example/smartpantrymanager/AddEditIngredientActivity.java
@@ -7,22 +7,14 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
-import android.content.Intent;
-import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class AddEditIngredientActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        Button buttonAddIngredient = findViewById(R.id.buttonAddIngredient);
-
-        buttonAddIngredient.setOnClickListener(v -> {
-            Intent intent = new Intent(MainActivity.this, AddEditIngredientActivity.class);
-            startActivity(intent);
-        });
+        setContentView(R.layout.activity_add_edit_ingredient);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
